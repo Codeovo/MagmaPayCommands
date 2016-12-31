6577454b3a8c13923a8291041fe53b8e9060b389
Mohammed Al-Dahleh <maldahleh@codeovo.io>
2016-12-30 21:37:38 -0500

    Main class framework

--- src/io/codeovo/mpcommands/MagmaPayCommands.java
+++ src/io/codeovo/mpcommands/MagmaPayCommands.java
@@ -1,4 +1,25 @@
 package io.codeovo.mpcommands;
 
-public class MagmaPayCommands {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MagmaPayCommands extends JavaPlugin {
+    private static MagmaPayCommands magmaPayCommands;
+
+    @Override
+    public void onEnable() {
+        getLogger().info("MagmaPay: Commands - Enabling...");
+        magmaPayCommands = this;
+
+        getLogger().info("MagmaPay: Commands - Enabled.");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("MagmaPay: Commands - Disabling...");
+        magmaPayCommands = null;
+
+        getLogger().info("MagmaPay: Commands - Disabled.");
+    }
+
+    public static MagmaPayCommands getInstance() { return magmaPayCommands; }
 }
